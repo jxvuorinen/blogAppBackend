7b911b8a20c59d97d44fc5171db8b743d0526919
Johanna Vuorinen <cjovu001@edu.xamk.fi>
2024-01-11 22:50:14 +0200

    prepare for heroku

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,8 @@
 const info = (...params) => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log = function () { }
+  }
+
   if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
   }
