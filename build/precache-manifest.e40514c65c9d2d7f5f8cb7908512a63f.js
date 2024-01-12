c7e43d3240c22b638d397366f7d9b35fcc4cec66
Johanna Vuorinen <cjovu001@edu.xamk.fi>
2024-01-12 23:07:06 +0200

    prepare for production

---
+++ build/precache-manifest.e40514c65c9d2d7f5f8cb7908512a63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d857ed7aad03536dbc66f8ada0bd9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3493ac891fcae57f0d3",
+    "url": "/static/css/main.c93dd643.chunk.css"
+  },
+  {
+    "revision": "7e9de1fb478538a075b3",
+    "url": "/static/js/2.e5fedf5e.chunk.js"
+  },
+  {
+    "revision": "b1f13ebe33e8a710e4b4400a9fb7a5c3",
+    "url": "/static/js/2.e5fedf5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3493ac891fcae57f0d3",
+    "url": "/static/js/main.110322bd.chunk.js"
+  },
+  {
+    "revision": "3bc2d6137c1f0f5c461a",
+    "url": "/static/js/runtime-main.d8c8d350.js"
+  }
+]);
